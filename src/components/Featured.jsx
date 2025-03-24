4af7bc94bf3ea9604d87a9ac2ef5023a590f951b
Inshalmunaf <inshalmunaf@gmail.com>
2025-03-24 23:56:13 +0500

    .

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -21,10 +21,10 @@ function Featured() {
                             <img src="https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-1326x1101.png" alt="" />
                         </div>
                     </div>
-                    <div className="cardscontainer relative w-1/2 h-[75vh] bg-red-400  rounded-xl">
+                    <div className="cardscontainer relative w-1/2 h-[75vh]  rounded-xl">
                         <div className="card w-full h-full rounded-xl overflow-hidden  ">
                             <h1 style={{ fontFamily: 'FoundersGrotesk, sans-serif' }} className='absolute right-full top-[50%] translate-x-[37%] -translate-y-[50%] z-[9] text-8xl leading-none select-none'>
-                                {"VISE".split("").map((item, index) =>(
+                                {"WISE".split("").map((item, index) =>(
                                     <span key={index} >{item}</span>
                                 ))}
                             </h1>
